990883ea55731a0d4c3ba7eccaa4985ae6a6e28a
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2025-07-28 15:08:46 +0300

    Update autoWater.ino

--- autoWater.ino
+++ autoWater.ino
@@ -12,10 +12,40 @@
 //определение кнопки
 //#define but 6
 
+//время между поливами
 uint16_t timer_min = 300;
+
+//время сасого полива
 uint8_t water_sec = 5;
 
+//флаг для сигания светодиодом
+bool led_flag = 0;
+
+//включить насосы на время t
 void get_water(uint16_t t){
+  digitalWrite(pump_1, HIGH);
+  digitalWrite(pump_2, HIGH);
+  digitalWrite(pump_3, HIGH);
+  digitalWrite(pump_4, HIGH);
+  digitalWrite(led, HIGH);
+  delay(t);
+  digitalWrite(pump_1, LOW);
+  digitalWrite(pump_2, LOW);
+  digitalWrite(pump_3, LOW);
+  digitalWrite(pump_4, LOW);
+  difitalWrite(led, LOW);
+}
+
+//мигать светодиодом раз в секунду
+void led_blink(){
+  if(millis()%1000==0 && flag==0){
+    flag = true;
+    digitalWrite(led, HIGH);
+  }
+  if(millis()%1000==0 && flag==1){
+    flag = false;
+    digitalWrite(led, LOW);
+  }
 }
 
 void setup(){
@@ -25,7 +55,13 @@ void setup(){
   pinMode(pump_4, OUTPUT);
   pinMode(led, OUTPUT);
   //pinMode(but, INPUT);
+  get_water(water_sec);
 }
 
 void loop(){
+  if(millis()%(timer_min*60*1000)==0){
+    get_water(water_sec);
+  } else {
+    led_blink();
+  }
 }
